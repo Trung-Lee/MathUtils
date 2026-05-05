55f071e8ef2f20cc07a7011441367aaac8ee327f
TrungLee <tuantrungbeo@gmail.com>
2026-05-05 23:53:46 +0700

    test cache

--- src/main/java/MathUtils.java
+++ src/main/java/MathUtils.java
@@ -2,7 +2,7 @@ public class MathUtils {
     public int add(int a, int b) {
         return a + b;
     }
-
+    // test cache bai 6
     public int subtract(int a, int b) {
         return a - b;
     }
