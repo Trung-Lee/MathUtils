575ceefbabf86dac0dbf523da41087a227039159
TrungLee <tuantrungbeo@gmail.com>
2026-05-05 22:15:35 +0700

    Add MathUtils + check

---
+++ src/test/java/MathUtilsTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MathUtilsTest {
+    @Test
+    public void testAdd() {
+        MathUtils math = new MathUtils();
+        assertEquals(5, math.add(2, 3));
+    }
+}
